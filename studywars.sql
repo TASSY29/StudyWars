04070847b3b78f851b2dc7cf46f3fd9ec1fa256b
PRISSILLA TASSY BINIT <tassy991229@users.noreply.github.com>
2021-01-27 00:19:18 +0800

    Update

--- studywars.sql
+++ studywars.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2018 at 02:18 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
